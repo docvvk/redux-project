d24bceb1d3aa31a681fabbe131080febb6e7f8e6
Vivek Malhan <vivek.malhan@yahoo.com>
2019-06-13 20:07:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Posts from './Components/Posts'
+import Postform from './Components/Postform'
+
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{ backgroundColor: 'pink', padding: '1ch', color: 'navy', textTransform: 'upper', fontFamily: 'cursive' }}>
+          Redux Sample App Displaying data from json placeholder API
         </p>
+        </header>
+        <Postform />
+        <hr></hr>
+        <Posts  />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +27,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+
+      
+      {/* <Posts /> */}
     </div>
   );
 }
---
+++ src/Components/Postform.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+
+class Postform extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            body: ''
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        }
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(post)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data));
+    }
+    render() {
+        return (
+            <div className='postForm'>
+                <form onSubmit={this.onSubmit}>
+                <h3>ADD POST</h3>
+                    <div>
+                        <label>TITLE</label>
+                        <br />
+                        <input name='title' type='text'  onChange={this.onChange} value={this.state.name}></input>
+                    </div>
+                   <div>
+                        <label>Body</label>
+                        <br />
+                        <textarea name='body' onChange={this.onChange} value={this.state.body}></textarea>
+                    </div>
+                    <br />
+                    <button type='submit'>SUBMIT</button>
+                    <br />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Postform;
